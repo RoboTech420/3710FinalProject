dd7587f52bacf3f56f227f6f61d6d31fadddfad2
Justin Neal <ford_guy420@hotmail.com>
2023-03-17 21:41:17 -0400

    add functions, global testSize

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 import seaborn as sns
+
+
+# VARIABLES
+testSize = 0.25
 
 # FUNCTIONS
 def parseData(urlData):
@@ -25,9 +32,17 @@ def printColumns(df):
 		print(df[coloumn].value_counts())
 	return
 
-# VARIABLES
-trainingSize = 0.75
-dataSize = 1.00 - trainingSize #just need to adjust line above
+def decisionTree(df, col):
+	X = df.drop(col, axis=1)
+	y = df[col]
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testSize, random_state=42)
+	clf = DecisionTreeClassifier(random_state=42)
+	clf.fit(X_train, y_train)
+	y_pred = clf.predict(X_test)
+	accuracy = accuracy_score(y_test, y_pred)
+	print(f'Accuracy : {accuracy} {col}')
+	return
+
 
 # load data
 df = pd.read_csv('DataSets\Diabetes\diabetic_data.csv')
@@ -54,3 +69,9 @@ print(df)
 # Displays the heat map of the data
 sns.heatmap(df.corr(numeric_only=True))
 plt.show()
+
+#sns.lineplot(df)
+#plt.show()
+
+for column in df.columns:
+	decisionTree(df, column)
